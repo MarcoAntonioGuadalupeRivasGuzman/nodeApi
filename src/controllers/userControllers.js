05f827711c85e62cbaaebb70b21cb2f6c8892e5a
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-02 17:33:52 -0600

    add a new funtions for the crud of the users

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,9 @@
+
+
+export const getUsers = (req,res)=> res.send('obteniendo usuarios');
+
+export const createUsers = (req,res)=> res.send('creando usuarios');
+
+export const updateUsers = (req,res)=> res.send('actualizando usuarios');
+
+export const deleteUsers = (req,res)=> res.send('borrando usuarios');
