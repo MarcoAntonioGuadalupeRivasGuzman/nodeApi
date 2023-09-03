cc6120f0f3b7afd9264be3aa3aa457dd3690d267
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-03 15:22:13 -0600

    add a new function for select on user by the username, response a row or 404 if not exist the user

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -6,6 +6,17 @@ export const getUsers = async (req,res)=> {
     res.json(rows);
 };
 
+export const getUserByName = async (req, res)=> {
+    const user = req.params.name;
+    const [rows] = await poolConexion.query('SELECT * FROM login WHERE usuario=?',user);
+    if(rows.length<=0){
+        return res.status(404).json({
+            message: 'No se encontro el usuario'
+        });
+    }
+    res.json(rows[0]);
+}
+
 export const createUser = async (req,res)=> {
     const {user, nombre, pass, tipo, recover} = req.body;
     const [rows] = await poolConexion.query('INSERT INTO login (usuario, nombre,password,tipo,recover ) VALUES (?,?,?,?,?)', [user, nombre, pass, tipo, recover]);
