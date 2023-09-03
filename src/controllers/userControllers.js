10fbc339996997d9961788d51b29102ee37a9737
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-02 19:23:52 -0600

    add a query for insert a user in the table login

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,8 +1,14 @@
+import { poolConexion } from "../conexion.js";
 
 
-export const getUsers = (req,res)=> res.send('obteniendo usuarios');
+export const getUsers = (req,res)=> {
+    res.send('obteniendo usuarios')
+};
 
-export const createUsers = (req,res)=> res.send('creando usuarios');
+export const createUser = async (req,res)=> {
+    const {user, nombre, pass, tipo, recover} = req.body;
+    const [rows] = await poolConexion.query('INSERT INTO login (usuario, nombre,password,tipo,recover ) VALUES (?,?,?,?,?)', [user, nombre, pass, tipo, recover]);
+    res.send({rows})};
 
 export const updateUsers = (req,res)=> res.send('actualizando usuarios');
 
