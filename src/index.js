da85c40939b1fad5ea7dc9c8f2a78afb2be50972
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-09 18:00:11 -0600

    add corse functions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const app= express();
 
 app.use(express.json());
 
+app.use((req, res, next)=>{
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.get('/test', async (req, res)=>{
     const [result] = await poolConexion.query('SELECT 1+1 AS result')
     res.json(result);
