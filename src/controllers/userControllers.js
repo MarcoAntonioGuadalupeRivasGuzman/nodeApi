1d69ca098679c0e216d89abc5f5e87cf6840903d
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-11 19:35:09 -0600

    add error handler

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -40,6 +40,7 @@ export const createUser = async (req,res)=> {
 
 export const updateUsers = async (req,res)=>{
     const {name} = req.params;
+    try{
     const {user, nombre, pass, tipo, recover} = req.body;
     console.log(user,nombre,pass,tipo,recover);
     const [result] = await poolConexion.query('UPDATE login SET usuario=?, nombre=?, password=?, tipo=?, recover=? WHERE usuario=?', [user,nombre,pass,tipo,recover, name]);
@@ -50,6 +51,9 @@ export const updateUsers = async (req,res)=>{
     }
     const [rows]= await poolConexion.query('SELECT * FROM login WHERE usuario=?',user);
     res.json(rows[0]);
+    }catch(error){
+        return res.status(500).json({message: error.message});
+    }
 } 
 
 export const deleteUsers = async (req,res)=>{
